b02336296aad885b92bb0e62677a4b1adef289f5
tausva <tautvydas.svarinskas@gmail.com>
2024-01-27 20:45:00 +0200

    moved all scripts to scripts folder

---
+++ Assets/Scripts/AttackGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Assembly_CSharp;
+using Assets.Scripts;
+using Assets.Scripts.Enums;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    public class AttackGenerator : MonoBehaviour
+    {
+        [SerializeField] ShieldGenerator shieldGenerator;
+        [SerializeField] HealthBar healthBarBot;
+
+        private void Awake()
+        {
+        }
+
+        void Start()
+        {
+        }
+
+        void Update()
+        {
+        }
+
+        public void ClaculateDamage(List<Tag> cardTags)
+        {
+            var isDamageShouldBeMade = false;
+            var shieldTag = shieldGenerator.GetRoundDefenseShield();
+
+            Debug.Log(shieldTag.ToString());
+
+            foreach (var tag in cardTags)
+            {
+                if (shieldTag.Equals(tag))
+                {
+                    isDamageShouldBeMade = true;
+                    break; 
+                }
+            }
+
+            if (isDamageShouldBeMade)
+            {
+                healthBarBot.DamageHealthBar();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Repository.cs
@@ -0,0 +1,70 @@
+﻿using Assets.Scripts.Enums;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Repository
+    {
+        public Dictionary<string, string> GetJokesByLanguage(LanguageJoke languageJoke)
+        {
+            var jokes = new Dictionary<string, string>();
+
+            switch (languageJoke)
+            {
+                case LanguageJoke.English:
+                    jokes = GetDadJokesInEnglish();
+                    break;
+                case LanguageJoke.Lithuania:
+                    jokes = GetDadJokesInLithuania();
+                    break;
+
+            }
+
+            return jokes;
+        }
+
+        private Dictionary<string, string> GetDadJokesInEnglish()
+        {
+
+            var filePath = $"{Application.dataPath}/Data/EnglishDadJokes.csv";
+
+            var jokes = GetData(filePath);
+
+            return jokes;
+        }
+
+
+        private Dictionary<string, string> GetDadJokesInLithuania()
+        {
+            var filePath = $"{Application.dataPath}/Data/LithuaniaDadJokes.csv";
+
+            var jokes = GetData(filePath);
+
+            return jokes;
+        }
+
+        private Dictionary<string, string> GetData(string filePath)
+        {
+            var jokes = new Dictionary<string, string>();
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    if (parts.Length == 2)
+                    {
+                        string question = parts[0];
+                        string answer = parts[1];
+                        jokes.TryAdd(question, answer);
+                    }
+                }
+            }
+
+            return jokes;
+        }
+    }
+}
